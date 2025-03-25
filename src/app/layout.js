274d72cda232c99102e179e1ddc5677250939081
Derek Pethel <derekmogi@gmail.com>
2025-03-24 20:41:05 -0400

    favicon stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,10 +65,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Title tag - sometimes helps with favicon association */}
+        <title>Abraxas Innovations</title>
+        
         {/* Add required meta tags for proper mobile rendering */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        
+        {/* Direct favicon links to override any cached versions */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
